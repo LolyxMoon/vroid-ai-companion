3908386b0e65ce8755af22716566ff7069f6fe2b
Sofia <falopaf5@gmail.com>
2026-01-08 17:07:59 -0300

    a
    a

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://x.com/NyxBNB"
+            href="https://x.com/i/communities/2009356134382227781"
             target="_blank"
             rel="noopener noreferrer"
           >
